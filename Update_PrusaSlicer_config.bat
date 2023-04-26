2229db0efc3c736a34bc6f561138d6a4f8665927
Anghelos Coulon <angheloc@gmail.com>
2023-04-26 14:35:58 -0400

    First commit

---
+++ Update_PrusaSlicer_config.bat
@@ -0,0 +1,10 @@
+set repoauthor=anghelos
+set reponame=vanier-prusaslicer-config
+set remotesource=https://github.com/%repoauthor%/%reponame%.git
+
+git clone %remotesource%
+
+set source=%CD%/%reponame%
+set destination=%appdata%\PrusaSlicer
+
+robocopy %source% %destination% /e /move
