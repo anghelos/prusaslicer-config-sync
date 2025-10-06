f6258596dfaad262c06972179e838b91c9a4dd52
Anghelos Coulon <angheloc@gmail.com>
2025-10-06 13:10:56 -0400

    Check if PrusaSlicer is running, offer to kill process, launch after sync

--- Update_PrusaSlicer_config.bat
+++ Update_PrusaSlicer_config.bat
@@ -1,5 +1,16 @@
 mode con: cols=60 lines=16
+mode con: cols=60 lines=16
 @echo off
+echo:
+Set "prusaslicer=prusa-slicer.exe"
+tasklist /NH /FI "imagename eq %prusaslicer%" 2>nul |find /i "%prusaslicer%" >nul
+If errorlevel 1 (goto :update)
+Echo PrusaSlicer is running. Please close it or press any key to kill it.
+PAUSE
+tasklist /NH /FI "imagename eq %prusaslicer%" 2>nul |find /i "%prusaslicer%" >nul
+if not errorlevel 1 (taskkill /IM prusa-slicer.exe /F)
+
+:update
 echo:
 echo Updating Prusaslicer Config
 
@@ -21,11 +32,19 @@ set source=%CD%/%reponame%
 set destination=%appdata%\PrusaSlicer
 
 robocopy %source% %destination% /mir /move /xd %source%\snapshots\
+echo:
+echo:
+ECHO: & ECHO -------------- & ECHO: & ECHO PrusaSlicer Config Updated! & echo: & ECHO -------------- & echo:
 
-ECHO: & ECHO -------------- & ECHO: & ECHO Prusaslicer Config Updated! & echo: & ECHO -------------- & echo:
-echo Cleaning up...
-timeout /t 1 /nobreak >nul
-@RD /Q /S %reponame%
+
+echo:
+echo Starting PrusaSlicer
+echo:
+echo:
+echo:
+start "" /b "C:/Program Files/Prusa3D/PrusaSlicer/prusa-slicer.exe"
+
+timeout /t 3
 
 exit /b 0
 
